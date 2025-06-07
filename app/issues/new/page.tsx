0f62c176715c9fca19a40132e46bbbbc4bb8260b
Ranjith K <thisisranjithk@gmail.com>
2025-06-07 04:47:56 -0700

    MarkdownEditor: react-simplemde-editor integ

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,16 @@
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+"use client";
+import { Button, TextField } from "@radix-ui/themes";
+import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 const NewIssue = () => {
   return (
     <div className="max-w-xl flex flex-col space-y-4">
       <TextField.Root placeholder="Title"></TextField.Root>
-      <TextArea placeholder="Description" size={"3"} />
+      <SimpleMDE placeholder="Description" />
       <div>
         <Button>Submit New Issue</Button>
       </div>
